b38a35a1075251dcb6222a209194df793d778f36
Jenny Massari <jennymassari@Jennys-MacBook-Pro.local>
2025-03-05 11:39:53 -0800

    Added Plotly library.

--- market_status_practice.py
+++ market_status_practice.py
@@ -1,11 +1,29 @@
 from IPython.display import display
 import pandas as pd
 
-table = pd.read_excel("Market_Status_Info_Report_2025-01-23.xlsx")
-display(table)
 
-market_column = table['Market']
-print(market_column)
+# Read and display the whole table
+data = pd.read_excel("Market_Status_Info_Report_2025-01-23.xlsx")
+display(data)
 
-type_column = table['Type']
-print(type_column)
+# Display all columns names
+all_columns = list(data.columns)
+# print(all_columns)
+
+# Sorting the columns with sorted() function
+sorted_columns = sorted(data.columns)
+print(sorted_columns)
+
+
+# Display only one column
+market_column = data['Market']
+# print(market_column)
+
+type_column = data['Type']
+# print(type_column)
+
+#Display multiple columns
+market_type_column = data[['Market', 'Type']]
+# print(market_type_column)
+
+
