ed9c35ec74a915341d58a8718a3a6854d2276e62
Jenny Massari <jennymassari@Jennys-MacBook-Pro.local>
2025-03-05 11:21:33 -0800

    Displaying the table by columns.

--- market_status_practice.py
+++ market_status_practice.py
@@ -3,3 +3,9 @@ import pandas as pd
 
 table = pd.read_excel("Market_Status_Info_Report_2025-01-23.xlsx")
 display(table)
+
+market_column = table['Market']
+print(market_column)
+
+type_column = table['Type']
+print(type_column)
